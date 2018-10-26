81f58440678517fc59cae367fe6b306e471ae8a8
Thomas Ebermann <thomas.ebermann@liip.ch>
2018-10-26 10:28:21 +0200

    stuff

--- scraper.py
+++ scraper.py
@@ -12,10 +12,10 @@ import lxml.html
 # root.cssselect("div[align='left']")
 #
 # # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
+scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
 #
 # # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+scraperwiki.sql.select("* from data where 'name'='peter'")
 
 # You don't have to do things with the ScraperWiki and lxml libraries.
 # You can use whatever libraries you want: https://morph.io/documentation/python
