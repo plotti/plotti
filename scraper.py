1e656dd15a518815b9a7b62af29701d9add92b86
Thomas Ebermann <thomas.ebermann@liip.ch>
2018-10-26 10:27:17 +0200

    stuff

--- scraper.py
+++ scraper.py
@@ -1,8 +1,8 @@
 # This is a template for a Python scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# import scraperwiki
-# import lxml.html
+import scraperwiki
+import lxml.html
 #
 # # Read in a page
 # html = scraperwiki.scrape("http://foo.com")
@@ -22,3 +22,4 @@
 # All that matters is that your final data is written to an SQLite database
 # called "data.sqlite" in the current working directory which has at least a table
 # called "data".
+html = scraperwiki.scrape("https://www.bergfex.ch/schweiz/")
